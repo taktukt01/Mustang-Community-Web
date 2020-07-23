3312bff5bd8857253ad59a2429323065d1c287c1
Taktuk <dodo@Pemas-MacBook-Pro.local>
2020-07-23 09:26:08 -0400

    gallery section finished

--- Lonyamship Website/js/scripts.js
+++ Lonyamship Website/js/scripts.js
@@ -19,9 +19,7 @@
         }
       }
     });
-    $('.carousel').carousel({
-      interval: 1700
-    });
+  
 
     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 
@@ -128,6 +126,36 @@ return code;
       offset: 80
     });
   
+
+    $('img[data-enlargable]').addClass('img-enlargable').click(function(){
+      var src = $(this).attr('src');
+      var modal;
+      function removeModal(){ 
+        
+        modal.remove(); 
+        $('body').off('keyup.modal-close'); }
+        
+      modal = $('<div>').css({
+          background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+          backgroundSize: 'contain',
+          width:'100%', height:'100%',
+          position:'fixed',
+          zIndex:'10000',
+          top:'0', left:'0',
+          cursor: 'zoom-out'
+      }).click(function(){
+removeModal();
+
+      }).appendTo('body');
+      //handling ESC
+      $('body').on('keyup.modal-close', function(e){
+        if(e.key==='Escape'){ 
+removeModal();
+         } 
+      });
+  });
+
+
     // Collapse Navbar
     var navbarCollapse = function() {
       if ($("#mainNav").offset().top > 100) {
