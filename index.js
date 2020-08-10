348621ed065c9984b0c2361630aea16610ba7f4c
taktuk <taktukgg@gmail.com>
2020-08-10 19:07:25 -0400

    getting json data from youtube api..

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
 const express = require("express");
-const path = require("path");
-
+const cors = require('cors');
+const morgan = require('morgan');
+const fetch = require('node-fetch');
 const app = express();
 
-
+require('dotenv').config();
 
 
 app.use(express.static('Lonyamship Website'));
 
 
 app.use('/', require('./routes/index'));
-// What is a middleware?
 
-// Will we need to use database at any point?
 
-// How to add user registration
+app.use(morgan('tiny'));
 
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require("stripe")("sk_test_51H4eyVBq0r2kQNIN20FfaWR9XFSxkWOQNDAtqG35QZQENhWsyLFrhC8EvX1HGDKolbHbXSFKaECbbtfrGyzalg2s00vPhq2PrE");
+app.use(cors());
 
-// Token is created using Stripe Checkout or Elements!
-// Get the payment token ID submitted by the form:
 
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd"
-  });
+app.get('/videos' , (req,res)=>{
+  const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL9ZK2cZDBoaz5S2B-TiPrN7sLvF6R4rP3&maxResults=50';
+fetch(`${url}&key=${process.env.GOOGLE_API_KEY}`)
+.then(response => response.json())
+.then(json => {
 
-  res.send({
-    clientSecret: paymentIntent.client_secret
-  });
+  res.json(json);
 });
 
 
+});
 
-  app.listen(3000, function () {
-    console.log("Express server listening on port 3000");
-    });
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found');
+  next(error);
+}
+
+function errorHandler(error, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: error.message
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
 
 
     
