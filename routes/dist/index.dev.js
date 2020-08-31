0a15004fb7c9cf51a5d6ed24b2a5b78e8b826eba
Taktuk <dodo@Pemas-MacBook-Pro.local>
2020-08-30 22:41:01 -0400

    added some html stuff for payment:

--- routes/dist/index.dev.js
+++ routes/dist/index.dev.js
@@ -8,20 +8,13 @@ var router = express.Router(); //  const path = require("path");
 var bodyParser = require('body-parser'); //  const pathFile = __dirname+'/Lonyamship Website/index.html';
 
 
-router.use(bodyParser());
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 var publishable_key = process.env.PUBLISHABLE_KEY;
-var secret_key_stripe = process.env.SECRET_KEY; // const stripe = require('stripe')(secret_key_stripe)
-// const session = await stripe.checkout.sessions.create({
-//   success_url: '',
-//   cancel_url: '',
-//   payment_method_types: ['card'],
-//   line_items: [
-//     {
-//       unit_amount:   ,
-//       quantity: 1},
-//   ],
-//   mode: 'payment',
-// });
+var secret_key_stripe = process.env.SECRET_KEY;
+
+var stripe = require('stripe')(secret_key_stripe);
 
 router.get("/", function (req, res) {
   res.render("index");
@@ -29,15 +22,87 @@ router.get("/", function (req, res) {
 router.get("/payment", function (req, res) {
   res.render("payment", {
     //data here
-    publish_key: publishable_key
+    publish_key: publishable_key,
+    donationAmount: 0
   });
+}); //  router.get("/formSuccess", function(req,res){
+//    res.render("formSuccess");
+//  });
+//  router.post("/payment", async function(req,res){
+//   console.log(JSON.stringify(req.body.donationAmount));
+//   res.render('payment', {
+//  donationAmount : req.body.donationAmount,
+//  publish_key : publishable_key 
+//   });
+//  });
+
+router.post("/payment", function _callee(request, response) {
+  var intent;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          console.log(request.body); //stripe accepts payments in the form of cents. What?!
+          // console.log(request.body.amountDonate * 100);
+          // Create the PaymentIntent
+
+          _context.next = 4;
+          return regeneratorRuntime.awrap(stripe.paymentIntents.create({
+            amount: request.body.amountDonate * 100,
+            currency: 'usd',
+            payment_method: request.body.payment_method_id,
+            // A PaymentIntent can be confirmed some time after creation,
+            // but here we want to confirm (collect payment) immediately.
+            confirm: true,
+            // If the payment requires any follow-up actions from the
+            // customer, like two-factor authentication, Stripe will error
+            // and you will need to prompt them for a new payment method.>
+            error_on_requires_action: true
+          }));
+
+        case 4:
+          intent = _context.sent;
+          return _context.abrupt("return", generateResponse(response, intent));
+
+        case 8:
+          _context.prev = 8;
+          _context.t0 = _context["catch"](0);
+
+          if (!(_context.t0.type === 'StripeCardError')) {
+            _context.next = 14;
+            break;
+          }
+
+          return _context.abrupt("return", response.send({
+            error: _context.t0.message
+          }));
+
+        case 14:
+          return _context.abrupt("return", response.status(500).send({
+            error: _context.t0.type
+          }));
+
+        case 15:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 8]]);
 });
-router.get("/formSuccess", function (req, res) {
-  res.render("formSuccess");
-});
-router.post("/payment", function (req, res) {
-  res.render('formSuccess', {
-    data: req.body
-  });
-});
+
+function generateResponse(response, intent) {
+  if (intent.status === 'succeeded') {
+    // Handle post-payment fulfillment
+    return response.send({
+      success: true
+    });
+  } else {
+    // Any other status would be unexpected, so error
+    return response.status(500).send({
+      error: 'Unexpected status ' + intent.status
+    });
+  }
+}
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ const bodyParser = require('body-parser');
 
 
 
-router.use(bodyParser());
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 
@@ -18,18 +20,8 @@ const publishable_key = process.env.PUBLISHABLE_KEY;
 const secret_key_stripe =  process.env.SECRET_KEY;
 
 
-// const stripe = require('stripe')(secret_key_stripe)
-// const session = await stripe.checkout.sessions.create({
-//   success_url: '',
-//   cancel_url: '',
-//   payment_method_types: ['card'],
-//   line_items: [
-//     {
-//       unit_amount:   ,
-//       quantity: 1},
-//   ],
-//   mode: 'payment',
-// });
+const stripe = require('stripe')(secret_key_stripe)
+
 
 
 router.get("/", (req,res)=>{
@@ -41,32 +33,83 @@ router.get("/", (req,res)=>{
   router.get("/payment", (req,res) =>{
     res.render("payment" , {
 //data here
-publish_key : publishable_key
-
-
+publish_key : publishable_key,
+donationAmount : 0 ,
 
     })
 
 });
 
+//  router.get("/formSuccess", function(req,res){
+//    res.render("formSuccess");
+//  });
+
+//  router.post("/payment", async function(req,res){
 
 
 
 
 
+//   console.log(JSON.stringify(req.body.donationAmount));
+//   res.render('payment', {
+//  donationAmount : req.body.donationAmount,
+//  publish_key : publishable_key 
 
 
- router.get("/formSuccess", function(req,res){
-   res.render("formSuccess");
- });
+//   });
 
- router.post("/payment", function(req,res){
-  res.render('formSuccess', {
- data : req.body ,
- 
+
+
+//  });
+
+router.post("/payment", async (request,response)=>{
+
+try {
+
+  console.log(request.body);
+
+
+  //stripe accepts payments in the form of cents. What?!
+  // console.log(request.body.amountDonate * 100);
+  // Create the PaymentIntent
+  let intent = await stripe.paymentIntents.create({
+    amount: request.body.amountDonate * 100, 
+    currency: 'usd',
+    payment_method: request.body.payment_method_id,
+
+    // A PaymentIntent can be confirmed some time after creation,
+    // but here we want to confirm (collect payment) immediately.
+    confirm: true,
+
+    // If the payment requires any follow-up actions from the
+    // customer, like two-factor authentication, Stripe will error
+    // and you will need to prompt them for a new payment method.>
+    error_on_requires_action: true,
 
   });
- });
+  return generateResponse(response, intent);
+} catch (e) {
+  if (e.type === 'StripeCardError') {
+    // Display error on client
+    return response.send({ error: e.message });
+  } else {
+    // Something else happened
+    return response.status(500).send({ error: e.type });
+  }
+}
+});
+
+
+
+function generateResponse(response, intent) {
+if (intent.status === 'succeeded') {
+  // Handle post-payment fulfillment
+  return response.send({ success: true });
+} else {
+  // Any other status would be unexpected, so error
+  return response.status(500).send({error: 'Unexpected status ' + intent.status});
+}
+}
 
 
  module.exports = router;
