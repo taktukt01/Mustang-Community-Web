629ea07234d691f03a0b4119a2aa75cb98f724aa
taktuk <taktukgg@gmail.com>
2020-11-25 18:42:15 -0500

    deleted some old branches

--- routes/galleryUpload.js
+++ routes/galleryUpload.js
@@ -19,7 +19,7 @@ var storage = multer.diskStorage({
 
 
   const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpg') {
         cb(null, true);
     } else {
         cb(null, false);
