279bed107bd592d5f232c686bf1a51135fdae384
Taktuk <taktukta@buffalo.edu>
2021-01-08 00:16:02 -0500

    form submission settings fixed

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -50,7 +50,7 @@ removeModal();
 
 
 
-//Get the button
+//scroll to top 
 var mybutton = document.getElementById("scrollToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -65,6 +65,51 @@ function scrollFunction() {
 }
 
 
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("contact-form");
+    var button = document.getElementById("submit-form");  //submit button
+    var status = document.getElementById("status-form"); // status -> fail or success
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Form Submitted!";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
 
 
 
