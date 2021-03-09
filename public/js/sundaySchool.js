9ca65ea78b614bd2d86f38b1030cce9262a438ac
TAKTUK <taktukta@buffalo.edu>
2021-03-09 10:27:55 -0500

    modified fonts

--- public/js/sundaySchool.js
+++ public/js/sundaySchool.js
@@ -1,42 +1,32 @@
-$(function(){
+$(function () {
     baguetteBox.run('.tz-gallery');
 
-    var scroll = new SmoothScroll('a[href*="#"]',{
+    var scroll = new SmoothScroll('a[href*="#"]', {
         speed: 500
     });
 
-
-    $("#moreInfoBtn").on('click', function(){
+    function toggleModals() {
         $(".culturalparagraph").toggle();
         $(".titleHeader").toggle();
+    }
 
-        
-    });
+    $("#moreInfoBtn").on('click', toggleModals());
 
-    $("#close").on('click', function(){
-        $(".culturalparagraph").toggle();
-        $(".titleHeader").toggle();
+    $("#close").on('click', toggleModals());
+    $("#ModalCloseButton").on('click', toggleModals());
+    $('button.close').on('click', toggleModals());
 
-    });
-    $("#ModalCloseButton").on('click', function(){
-        $(".culturalparagraph").toggle();
-        $(".titleHeader").toggle();
 
-    });
-    
-    
-    
     $('#sidebarCollapse').on('click', function () {
 
 
         // sidebar is just clicked
         // if it is already activethen show the content next to it.
-        if( $(this).hasClass('active')){
+        if ($(this).hasClass('active')) {
             $('.pageDetails').show();
-        }
-        else{
-        $('.pageDetails').hide();
-      
+        } else {
+            $('.pageDetails').hide();
+
         }
 
         $('#sidebar').toggleClass('active');
